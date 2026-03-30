1f71ea316853f6cdfe5d3387faf1e17ee9474ab7
Bezllkly <dd219406@gmail.com>
2026-03-31 00:15:47 +0300

    Add features in register

--- routers/chat.py
+++ routers/chat.py
@@ -6,17 +6,41 @@ from fastapi.staticfiles import StaticFiles
 import os
 from fastapi import APIRouter
 from typing import Optional
+import sql.sql as sql
+from sqlalchemy import URL, create_engine, text, insert, ForeignKey, select, update, func, cast, delete
+from sqlalchemy.orm import joinedload, selectinload
+from datetime import datetime, timezone, timedelta
+from pydantic import BaseModel
+import secrets
+
+class CreateSession(BaseModel):
+    username: str
+    password: str
 
 router = APIRouter(prefix='/chat',
                    tags=['chat'])
 
 @router.get('')
-async def chat(response: Response, session: Optional[str] = Cookie(default=None)):
-    if session:
-        response.set_cookie(
-            key="session",
-            value=session,
-            httponly=True,
-            secure=False
-        )
+async def chat(response: Response, session_value: Optional[str] = Cookie(default=None)):
+    if session_value:
+        async with sql.as_session() as session:
+            response = await session.execute(select(sql.Session).filter_by(token=session_value))
+            user = response.scalar_one_or_none()
+            if user:
+                if user.expires_at < datetime.now(timezone.utc):
+                    await session.execute(delete(sql.Session).filter_by(token=session_value))
+                else:
+                    user.expires_at = datetime.now(timezone.utc) + timedelta(days=30)
+                    return FileResponse('static/chat.html')
+            await session.commit()
+
     
+                
+@router.get('/registration')
+async def get_registration(session_value: Optional[str] = Cookie(default=None)):
+    if not session_value:
+        return FileResponse('static/register.html')
+
+@router.post('/api/registration')
+async def registration():
+    pass
--- sql/sql.py
+++ sql/sql.py
@@ -1,11 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase, Mapped, relationship, mapped_column, joinedload, selectinload, contains_eager
-from sqlalchemy import URL, create_engine, text, insert, ForeignKey, select, update, func, cast
+from sqlalchemy import URL, create_engine, text, insert, ForeignKey, select, update, func, cast, Enum
 from sqlalchemy import Table, Column, Integer, String, MetaData, Boolean, DateTime, BigInteger, ARRAY
-from datetime import datetime
-from enum import Enum
+from datetime import datetime, timedelta, timezone
+import enum
 from dotenv import load_dotenv
-import os
+import os, asyncio
 
 load_dotenv()
 DB_LINK = os.getenv('DBLINK')
@@ -17,21 +17,21 @@ engine = create_async_engine(
 
 as_session = async_sessionmaker(engine, expire_on_commit=False)
 
-class Chat_type(Enum):
+class Chat_type(enum.Enum):
     private = 'private'
     group = 'group'
     channel = 'channel'
 
-class Attach_type(Enum):
+class Attach_type(enum.Enum):
     file = 'file'
     capture = 'capture'
     video = 'video'
 
-class Mess_state(Enum):
+class Mess_state(enum.Enum):
     sent = 'sent'
     read = 'read'
 
-class Chat_roles(Enum):
+class Chat_roles(enum.Enum):
     member = 'member'
     admin = 'admin'
     owner = 'owner'
@@ -52,37 +52,61 @@ chat_members = Table(
     Base.metadata,
     Column('chat_id', ForeignKey('chats.id'), primary_key=True),
     Column('user_id', ForeignKey('users.id'), primary_key=True),
-    Column('role', Chat_roles, default=Chat_roles.member),  # 'owner', 'admin', 'member'
-    Column('joined_at', DateTime, default=datetime.utcnow)
+    Column('role', Enum(Chat_roles), default=Chat_roles.member),  # 'owner', 'admin', 'member'
+    Column('joined_at', DateTime, default=datetime.now(timezone.utc))
 )
 
 class User(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    userid: Mapped[int] = mapped_column(unique=True)
+    user_id: Mapped[int] = mapped_column(unique=True)
     username: Mapped[str] = mapped_column(unique=True)
     name: Mapped[str] = mapped_column()
     password: Mapped[str] = mapped_column()
+    avatar = Column(String(100), nullable=True)
 
-    avatar = mapped_column()
     is_active = mapped_column(Boolean, default=True)
-    last_seen = mapped_column(DateTime, default=datetime.utcnow)
-    created_at = mapped_column(DateTime, default=datetime.utcnow)
+    last_seen = mapped_column(DateTime, default=datetime.now(timezone.utc))
+    created_at = mapped_column(DateTime, default=datetime.now(timezone.utc))
 
     chats = relationship("Chat", secondary=chat_members, back_populates="members")
     chatsadmin = relationship('Chat', back_populates='creator', cascade="all, delete-orphan")
     messages = relationship("Message", back_populates='user')
+    sessions = relationship("Session", back_populates='user', cascade="all, delete-orphan")
+
+class Session(Base):
+    __tablename__ = 'sessions'
+
+    id = Column(Integer, primary_key=True)
+    token = Column(String(64), unique=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    expires_at = Column(DateTime, default=datetime.now(timezone.utc) + timedelta(days=30), onupdate=datetime.now(timezone.utc) + timedelta(days=30))
+    user_agent = Column(String(255), nullable=True)
+    ip_address = Column(String(15), nullable=True) 
+    user = relationship('User', back_populates='sessions')
+
+class Username(Base):
+    __tablename__ = 'usernames'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), unique=True)
+    owner_type = Column(Enum(Chat_type), default=Chat_type.private)
+    owner_id = Column(Integer, unique=True)
 
 class Chat(Base):
     __tablename__ = 'chats'
 
     id = Column(Integer, primary_key=True)
-    type = Column(Chat_type, default=Chat_type.private)
+    username = Column(Integer, unique=True)
+    type = Column(Enum(Chat_type), default=Chat_type.private)
     title = Column(String(50), nullable=True)
     description = Column(String(200), nullable=True)
     created_by = Column(Integer, ForeignKey('users.id'), nullable=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    avatar = Column(String(100), nullable=True)
 
     creator = relationship('User', back_populates='chatsadmin', foreign_keys=[created_by])
     members = relationship('User', secondary=chat_members, back_populates='chats')
@@ -94,13 +118,13 @@ class Message(Base):
     id = Column(Integer, primary_key=True)
     chat_id = Column(Integer, ForeignKey('chats.id', ondelete='CASCADE'), index=True)
     created_by = Column(Integer, ForeignKey('users.id', ondelete='SET NULL'), index=True)
-    reply_to_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    reply_to_id = Column(Integer, ForeignKey('messages.id'), nullable=True, index=True)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
     text = Column(String, nullable=True)
-    attachment_type = Column(Attach_type, nullable=True)
+    attachment_type = Column(Enum(Attach_type), nullable=True)
     attachment = Column(String, nullable=True)
-    state = Column(Mess_state, default=Mess_state.sent)
+    state = Column(Enum(Mess_state), default=Mess_state.sent)
     viewed_by = Column(ARRAY(Integer), default=[])
     count_viewed = Column(Integer, default=1)
 
@@ -108,3 +132,18 @@ class Message(Base):
     chat = relationship('Chat', back_populates='messages', foreign_keys=[chat_id])
     reply_to = relationship('Message', remote_side=[id], foreign_keys=[reply_to_id])
     replies = relationship('Message', back_populates='reply_to')
+
+
+
+'''
+UPDARE DATABASES
+'''
+async def update_all():
+    async with engine.connect() as con:
+        await con.run_sync(Base.metadata.drop_all)
+        await con.run_sync(Base.metadata.create_all)
+        await con.commit()
+    print('updated succesfully')
+
+if __name__ == "__main__":
+    asyncio.run(update_all())
